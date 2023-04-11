dee005f32847475c5119610e8af7caa1fa69b5b4
kannanchu <krishangbro@gmail.com>
2023-04-10 20:13:43 -0700

    Initial commit

---
+++ Download_data.py
@@ -0,0 +1,35 @@
+# @title code for downloading the options data.
+
+import pandas as pd
+
+payload = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+first_table = payload[0]
+second_table = payload[1]
+
+df1 = first_table
+symbols = df1['Symbol'].values.tolist()
+symbols.remove('BRK.B')
+symbols.remove('BF.B')
+
+from yahooquery import Ticker
+
+t = Ticker(symbols, asynchronous=True)
+
+df2 = t.option_chain
+
+
+df2.index.unique(level=0)
+
+
+from datetime import datetime
+import csv
+dtt = datetime.today().strftime('%Y-%m-%d')
+today = datetime.strptime(dtt, "%Y-%m-%d").date()
+print(today)
+df2.to_csv('optionschain1.csv')
+with open('optionschain1.csv', 'r') as f_in, open(r'C:\Users\krish\OneDrive\Desktop\optionschain.csv', 'w',newline='') as f_out:
+  reader = csv.reader(f_in)
+  writer = csv.writer(f_out)
+  for row in reader:
+    writer.writerow(row)
+
